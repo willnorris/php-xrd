4bb6a15088bb9747a8bf3afde6bd33104e5e96ba
Will Norris <will@willnorris.com>
2009-03-06 16:08:54 -0800

    add discovery method for Link response header

---
+++ Discovery/Link_Header.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once 'Discovery.php';
+require_once 'Discovery/Link.php';
+require_once 'Discovery/Method.php';
+
+
+/**
+ * Discovery Method that uses "Link" HTTP Response header
+ *
+ * @see http://www.ietf.org/internet-drafts/draft-nottingham-http-link-header-04.txt
+ */
+class Discovery_Link_Header implements Discovery_Method {
+
+
+	public static function discover($uri) {
+
+		$ch = curl_init($uri);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_NOBODY, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'discovery/1.0 (php)');
+
+		$content = curl_exec($ch);
+		curl_close($ch);
+
+		return self::parse($content);
+	}
+
+
+	/**
+	 * Parse the given HTTP response headers.
+	 *
+	 * TODO this method does not currently handle the case where multiple 
+	 * header values are included on a single line.
+	 *
+	 * @param string $content HTTP response headers
+	 * @return array array of Discovery_Link objects
+	 */
+	public static function parse($content) {
+		$links = array();
+
+		$headers = explode("\r\n", $content);
+
+		// remove HTTP response code
+		array_shift($headers);
+
+		// normalize headers that are split over multiple lines
+		for ($i=(sizeof($headers)-1); $i>=0; $i--) {
+			if ( strpos($headers[$i], ' ') === 0 || strpos($headers[$i], "\t") === 0 ) {
+				$headers[$i-1] .= preg_replace('/^\s+/', '', $headers[$i]);
+				unset($headers[$i]);
+			}
+		}
+
+		foreach ($headers as $header) {
+			if (empty($header)) continue;
+
+			list ($name, $value) = explode(':', $header, 2);
+			$name = trim($name);
+			$value = trim($value);
+
+			// we only care about "link" headers
+			if (strcasecmp($name, 'link') != 0) continue;
+
+			if ( $link = Discovery_Link::from_header($value) ) {
+				$links[] = $link;
+			}
+		}
+
+		return $links;
+	}
+
+}
+
+?>
--- tests/DiscoveryTest.php
+++ tests/DiscoveryTest.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once dirname(__FILE__) . '/TestCase.php';
-require_once 'Discovery/HostMeta.php';
+require_once 'Discovery/Host_Meta.php';
+require_once 'Discovery/Link_Header.php';
  
 class DiscoveryTest extends Discovery_TestCase {
 
@@ -19,6 +20,35 @@ class DiscoveryTest extends Discovery_TestCase {
 		// test 2
 		$content = file_get_contents($this->data_dir . 'host-meta-2');
 		$links = Discovery_Host_Meta::parse($content);
+
+		$this->assertEquals(2, sizeof($links));
+
+		$this->assertEquals('http://openxrd.org/sitemap.xml', $links[0]->uri);
+		$this->assertEquals(1, sizeof($links[0]->rel));
+		$this->assertEquals('index', $links[0]->rel[0]);
+
+		$this->assertEquals('http://openxrd.org/xrd.xml', $links[1]->uri);
+		$this->assertEquals('application/xrd+xml', $links[1]->type);
+		$this->assertEquals(3, sizeof($links[1]->rel));
+		$this->assertTrue(in_array('describedby', $links[1]->rel));
+		$this->assertTrue(in_array('alternate', $links[1]->rel));
+		$this->assertTrue(in_array('http://example.com/custom/rel', $links[1]->rel));
+	}
+
+	public function testLinkHeader() {
+		// test 1
+		$content = file_get_contents($this->data_dir . 'http-headers-1.txt');
+		$links = Discovery_Link_Header::parse($content);
+
+		$this->assertEquals(1, sizeof($links));
+		$this->assertEquals('http://openxrd.org/xrd.xml', $links[0]->uri);
+		$this->assertEquals('application/xrd+xml', $links[0]->type);
+		$this->assertEquals(1, sizeof($links[0]->rel));
+		$this->assertEquals('describedby', $links[0]->rel[0]);
+
+		// test 2
+		$content = file_get_contents($this->data_dir . 'http-headers-2.txt');
+		$links = Discovery_Link_Header::parse($content);
 
 		$this->assertEquals(2, sizeof($links));
 
