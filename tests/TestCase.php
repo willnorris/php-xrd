f3e2ae5425c753d98b72fd4c57d91d922c207f6b
Will Norris <will@willnorris.com>
2009-03-20 13:24:54 -0700

    move code into 'src' directory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'DISCOVERY_ROOT', dirname(dirname(__FILE__)) );
-set_include_path(DISCOVERY_ROOT . PATH_SEPARATOR . get_include_path());
+set_include_path(DISCOVERY_ROOT . '/src' . PATH_SEPARATOR . get_include_path());
 
 error_reporting(E_ALL);
 ini_set('display_errors', true);
