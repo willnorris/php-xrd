14f64c3f581089bcb38d7f076f738a443aeb95ff
Will Norris <will@willnorris.com>
2009-03-26 16:40:16 -0700

    add Yadis test to all test suite

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,6 +4,9 @@ require_once dirname(__FILE__) . '/Discovery/LRDD/ParseTest.php';
 require_once dirname(__FILE__) . '/Discovery/LRDD/LinkPatternTest.php';
 require_once dirname(__FILE__) . '/Discovery/LRDD/FetchTest.php';
 
+require_once dirname(__FILE__) . '/Discovery/Yadis/ParseTest.php';
+require_once dirname(__FILE__) . '/Discovery/Yadis/FetchTest.php';
+
 require_once dirname(__FILE__) . '/XRD/TemplateURITest.php';
 require_once dirname(__FILE__) . '/XRD/ParserTest.php';
 
@@ -18,6 +21,9 @@ class AllTests extends PHPUnit_Framework_TestSuite
 		$suite->addTestSuite('Discovery_LRDD_ParseTest');
 		$suite->addTestSuite('Discovery_LRDD_LinkPatternTest');
 		$suite->addTestSuite('Discovery_LRDD_FetchTest');
+
+		$suite->addTestSuite('Discovery_Yadis_ParseTest');
+		$suite->addTestSuite('Discovery_Yadis_FetchTest');
 
 		$suite->addTestSuite('XRD_TemplateURITest');
 		$suite->addTestSuite('XRD_ParserTest');
